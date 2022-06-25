ca0fd78708dfeb66f1ce69be13edcfeb6a4242f7
varmaharsh <varmaharsh15@gmail.com>
2022-06-25 20:20:06 +0530

    commit next app

---
+++ next-app/constants/index.js
@@ -0,0 +1,69 @@
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "_maxWhitelistedAddresses",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "addAddressToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxWhitelistedAddresses",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numAddressesWhitelisted",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "whitelistedAddresses",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x82f2B45182d2Dcf6496aDD2759689b7b70fc6E47";
+
+export const test = "";
